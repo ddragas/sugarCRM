1841422c86818529d86732fd73838c20abd499cb
ddragas <dejan.dragas@myzone.com>
2018-04-08 20:10:46 +0200

    initial commit

---
+++ config_override.php
@@ -0,0 +1,6 @@
+<?php
+/***CONFIGURATOR***/
+$sugar_config['default_date_format'] = 'd.m.Y';
+$sugar_config['default_decimal_seperator'] = ',';
+$sugar_config['default_time_format'] = 'H.i';
+/***CONFIGURATOR***/
---
+++ custom/modules/Connectors/metadata/display_config.php
@@ -0,0 +1,23 @@
+<?php
+// created: 2018-04-08 17:55:48
+$modules_sources = array (
+  'Accounts' => 
+  array (
+    'ext_rest_insideview' => 'ext_rest_insideview',
+  ),
+  'Contacts' => 
+  array (
+    'ext_rest_insideview' => 'ext_rest_insideview',
+  ),
+  'Leads' => 
+  array (
+    'ext_rest_insideview' => 'ext_rest_insideview',
+  ),
+  'Prospects' => 
+  array (
+  ),
+  'Opportunities' => 
+  array (
+    'ext_rest_insideview' => 'ext_rest_insideview',
+  ),
+);
